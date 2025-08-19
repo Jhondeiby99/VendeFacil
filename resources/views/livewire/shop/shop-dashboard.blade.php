fdb9b334fc4ab7ae72eba8769cadc5e1d48f8dbd
john deiby moreno benavies <107939315+Jhondeiby99@users.noreply.github.com>
2025-08-19 12:35:19 -0500

    Update shop-dashboard.blade.php

--- resources/views/livewire/shop/shop-dashboard.blade.php
+++ resources/views/livewire/shop/shop-dashboard.blade.php
@@ -11,7 +11,7 @@
         <!-- Botón flotante para abrir carrito -->
     <flux:modal.trigger class="" :name="'cart-modal'">
         <flux:button  x-data="" x-on:click.prevent="$dispatch('open-modal', 'cart-modal')"
-                    class="fixed bottom-4 right-4 rounded-full w-16 h-16 flex items-center justify-center shadow-lg">
+                    class="items-center font-medium justify-center gap-2 whitespace-nowrap disabled:opacity-75 dark:disabled:opacity-75 disabled:cursor-default disabled:pointer-events-none h-10 text-sm rounded-lg px-4 inline-flex bg-white hover:bg-zinc-50 dark:bg-zinc-700 dark:hover:bg-zinc-600/75 text-zinc-800 dark:text-white border border-zinc-200 hover:border-zinc-200 border-b-zinc-300/80 dark:border-zinc-600 dark:hover:border-zinc-600 shadow-xs">
             🛒 {{ count($cart) }}
         </flux:button>
     </flux:modal.trigger>
